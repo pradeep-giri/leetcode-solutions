f18495686344b6439f5c45745367efb7f452a7ef
Pradeep Giri <pradeepgiri.pg123@gmail.com>
2024-09-30 17:48:05 -0700

    Time: 1 ms (99.87%), Space: 45.8 MB (50.07%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,14 +3,12 @@ class Solution {
         int i = 0;
         for(int j = 0; j < nums.length; j++) {
             if(nums[j] != 0) {
-                nums[i] = nums[j];
-                i++;
+                nums[i++] = nums[j];
             }
         }
 
         while(i < nums.length) {
-            nums[i] = 0;
-            i++;
+            nums[i++] = 0;
         }
     }
 }
