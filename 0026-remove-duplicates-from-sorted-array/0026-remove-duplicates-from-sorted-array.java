8e3b78acacd87c355210160d25d12e0a387ab43e
Pradeep Giri <pradeepgiri.pg123@gmail.com>
2024-10-17 04:45:43 -0700

    Time: 0 ms (100%), Space: 44.4 MB (88.76%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,7 +3,7 @@ class Solution {
         int i = 1;
 
         for(int j = 1; j < nums.length; j++) {
-            if(nums[j] != nums[j-1]) {
+            if(nums[j] != nums[i-1]) {
                 nums[i] = nums[j];
                 i++;
             }
